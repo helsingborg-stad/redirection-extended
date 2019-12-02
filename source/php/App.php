67116c55d7d288a785ae3a553419ba2721670af2
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2019-12-02 16:01:53 +0100

    Clean up boilerplate code

--- source/php/App.php
+++ source/php/App.php
@@ -5,26 +5,5 @@ namespace RedirectionExtended;
 class App
 {
     public function __construct()
-    {
-        add_action('admin_enqueue_scripts', array($this, 'enqueueStyles'));
-        add_action('admin_enqueue_scripts', array($this, 'enqueueScripts'));
-    }
-
-    /**
-     * Enqueue required style
-     * @return void
-     */
-    public function enqueueStyles()
-    {
-        wp_register_style('redirection-extended-css', REDIRECTIONEXTENDED_URL . '/dist/' . \RedirectionExtended\Helper\CacheBust::name('css/redirection-extended.css'));
-    }
-
-    /**
-     * Enqueue required scripts
-     * @return void
-     */
-    public function enqueueScripts()
-    {
-        wp_register_script('redirection-extended-js', REDIRECTIONEXTENDED_URL . '/dist/' . \RedirectionExtended\Helper\CacheBust::name('js/redirection-extended.js'));
-    }
+    { }
 }
