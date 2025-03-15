3221b6700c6fe86ee2a9ed4a6bd792b5b6a4c3a4
GitHub Action <>
2025-03-15 09:51:17 +0000

    build: update package file versions to 3.0.5

--- redirection-extended.php
+++ redirection-extended.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Redirection Extended
  * Plugin URI:        https://github.com/helsingborg-stad/redirection-extended
  * Description:       Extends the Wordpress Redirection plugin
- * Version: 3.0.4
+ * Version: 3.0.5
  * Author:            Nikolas Ramstedt
  * Author URI:        https://github.com/nramstedt
  * License:           MIT
